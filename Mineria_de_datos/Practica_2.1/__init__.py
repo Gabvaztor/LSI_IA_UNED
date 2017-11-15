bc57fe2154316877363db601ea5e6d736f349390
Gabriel Vázquez <gabvaztor@gmail.com>
2017-11-15 22:41:01 +0100

    Create 100 instances. TODO Strong and weak methods.

--- Mineria_de_datos/Practica_2.1/__init__.py
+++ Mineria_de_datos/Practica_2.1/__init__.py
@@ -1,1 +1,68 @@
+"""
+Práctica a realizar:
+
+El estudiante generará un conjunto de datos artificial compuesto por 100 instancias caracterizadas por una variable
+relevante en sentido fuerte, tres variables relevantes en sentido débil y una variable totalmente irrelevante. Esta 
+última se puede generar mediante números aleatorios extraídos de una distribución de probabilidad uniforme o normal
+(gaussiana). Como indicación sugerimos extender el ejemplo XOR a tres dimensiones. A continuación, aplicará
+diferentes técnicas de selección de variables disponibles en weka (un mínimo de tres de filtrado, el análisis
+de componentes principales y la técnica de envoltura, WrapperSubsetEval, con BayesNet como clasificador y 
+empleando todos los valores por defecto, salvo el número máximo de padres que se debe modificar a 3).
+
+"""
+
+import numpy as np
+import pandas as pd
+import arff
+from UtilsFunctions import *
 # Crear 100 instancias de datos para S={X1,X2,X3,X4,X5} con valores Booleanos
+
+
+# Existen 16 posibles instancias si extendemos el problema XOR a tres dimensiones.
+# Trabajamos con el siguiente conjunto:
+X1 = [0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1]
+X2 = [0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1]
+X3 = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+X4 = [0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0]
+X5 = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
+# Y = X1 XOR X2 XOR X3 (Problema XOR con 3 dimensiones)
+# Se cumple la propiedad conmutativa.
+Y = np.logical_xor(X1,np.logical_xor(X2,X3))
+
+# Y = X1 OR (X1 XOR X2) OR (X1 XOR X3) OR (X1 XOR X4)
+
+
+# X4 = X2 XOR X3
+#X4 = np.logical_xor(X2,X3)
+# Vamos a trabajar de tal forma que X1 sea fuertemente relevante, es decir, que si se elimina esa característica
+# por sí sola, va a deteriorar el rendimiento del clasificador de Bayes óptimo.
+
+# Como se nos pide 100 instancias (con 0 y 1), creamos las 84 restantes con el siguiente método:
+def create_x_instances_to_list(number_of_instances, list_to_add):
+    new_list = list(np.random.randint(2, size=number_of_instances))
+    list_to_return = list_to_add + new_list
+    # Necesario para que no sean valores numpy
+    x = [int(elem) for elem in list_to_return]
+    return x
+
+X1 = create_x_instances_to_list(84,X1)
+X2 = create_x_instances_to_list(84,X2)
+X3 = create_x_instances_to_list(84,X3)
+X4 = create_x_instances_to_list(84,X4)
+X5 = create_x_instances_to_list(84,X5)
+
+def create_columns_per_list():
+    df = zip(X1, X2, X3, X4, X5)
+    return df
+
+create_columns_per_list()
+
+# Convertimos los datos a .arff para poder leerlos en weka con la siguiente función
+def convert_lists_to_arff(lists):
+    x = arff.dumps(lists,
+                   relation="arff",
+                   names=['X1','X2','X3','X4','X5'])
+    pt(x)
+    write_json_to_pathfile(x,"D:\\result.arff")
+
+convert_lists_to_arff(create_columns_per_list())
---
+++ UtilsFunctions.py
@@ -0,0 +1,37 @@
+import os
+def pt(title=None, text=None):
+    """
+    Use the print function to print a title and an object coverted to string
+    :param title:
+    :param text:
+    """
+    if text is None:
+        text = title
+        title = "------------------------"
+    else:
+        title += ':'
+    print(str(title) + " \n " + str(text))
+
+def write_json_to_pathfile(object, filepath):
+    """
+    Write a string to a path file
+    :param string: string to write
+    :param path: path where write
+    """
+    try:
+        create_directory_from_fullpath(filepath)
+        with open(filepath, 'w+') as file:
+            # file = open(filepath, 'w+')
+            file.write(str(object))
+    except:
+        raise ValueError("Error")
+
+def create_directory_from_fullpath(fullpath):
+    """
+    Create directory from a fullpath if it not exists.
+    """
+    # TODO (@gabvaztor) Check errors
+    directory = os.path.dirname(fullpath)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
