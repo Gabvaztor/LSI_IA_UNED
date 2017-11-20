65793ebcf852b8dbd00bd570c6197f439eca5112
Gabriel Vázquez <gabvaztor@gmail.com>
2017-11-20 23:42:33 +0100

    Calculating relevants 3

--- Mineria_de_datos/Practica_2.1/__init__.py
+++ Mineria_de_datos/Practica_2.1/__init__.py
@@ -74,51 +74,51 @@ data = create_columns_per_list()
 
 def calculate_subsets_sx(array, possibilities, Y):
     s1, s2, s3, s4, s5 = [], [], [], [], []
-    xs1, xs2, xs3, xs4, xs5 = [], [], [], [], []
+    all_rows = []
     p1_X1, p1_X2, p1_X3, p1_X4, p1_X5 = False
+    add_to_all_rows = True
     for string_feature, possibilities in possibilities.items():
         for i in range(len(data)):
             row = array[i]
             y = Y[i]
+            if add_to_all_rows:
+                all_rows.append(list(row).append(y))
             if string_feature == 'x1':
-                xs1.append(list(row).append(y))
                 s1.append(list(row[1:5]))
                 if p1_X1 is False:
                     if row[0] == possibilities[0] or row[0] == possibilities[1]:
                         p1_X1 = True
             elif string_feature == 'x2':
-                xs2.append(list(row).append(y))
                 s2.append(list(np.hstack([row[0], row[2:5]])))
                 if p1_X2 is False:
                     if row[1] == possibilities[0] or row[1] == possibilities[1]:
                         p1_X2 = True
             elif string_feature == 'x3':
-                xs3.append(list(row).append(y))
                 s3.append(list(np.hstack([row[0:2], row[3:5]])))
                 if p1_X3 is False:
                     if row[2] == possibilities[0] or row[2] == possibilities[1]:
                         p1_X3 = True
                 # pt(str(i + 1), sx)
             elif string_feature == 'x4':
-                xs4.append(list(row).append(y))
                 s4.append(list(np.hstack([row[0:3], row[4]])))
                 if p1_X4 is False:
                     if row[3] == possibilities[0] or row[3] == possibilities[1]:
                         p1_X4 = True
             elif string_feature == 'x5':
-                xs5.append(list(row).append(y))
+
                 s5.append(list(row[0:4]))
                 if p1_X5 is False:
                     if row[4] == possibilities[0] or row[4] == possibilities[1]:
                         p1_X5 = True
-    return xs1, xs2, xs3, xs4, xs5, s1, s2, s3, s4, s5, p1_X1, p1_X2, p1_X3, p1_X4, p1_X5
-strong_relevant(y,x,si,)
+    return all_rows, s1, s2, s3, s4, s5, p1_X1, p1_X2, p1_X3, p1_X4, p1_X5
+
+
 
 def find_relevants_information(data, Y, possibilities):
     """
     Encuentra los datos que son relevantes fuertes, débiles o irrelevantes.
     """
-    # p1 = p(Xi=xi,Si=si)>0
+    # p1 = p(Xi=xi,Si=si)>0 para all xi
     # p2 = p(Y=y | Xi=xi, Si=si)
     # p3 = p(Y=y | Si=si)
 
@@ -129,8 +129,8 @@ def find_relevants_information(data, Y, possibilities):
     pt(len(data[0]))
     array_data = np.asarray(data)
     # All subsets sx
-    xs1, xs2, xs3, xs4, xs5, s1, s2, s3, s4, s5, p1_X1, p1_X2, p1_X3, p1_X4, p1_X5 = \
-        calculate_subsets_sx(array_data, possibilities,Y)
+    all_rows, s1, s2, s3, s4, s5, p1_X1, p1_X2, p1_X3, p1_X4, p1_X5 = calculate_subsets_sx(array_data, possibilities,Y)
+    # Todo si para débil debe coger subconjuntos
     # Count numbers of times apper
     for string_feature, possibilities in possibilities.items():
         for i in range(len(data)):
@@ -149,7 +149,7 @@ def find_relevants_information(data, Y, possibilities):
                 pass
 
 possibilities = {'x1':[0,1], 'x2':[0,1], 'x3':[0,1], 'x4':[0,1], 'x5':[0,1]}
-
-find_relevants_information(data=data, y=Y, possibilities=possibilities)
+y = [0,1]
+find_relevants_information(data=data, Y=Y, possibilities=possibilities)
 # Creamos archivo arff
 #convert_lists_to_arff(create_columns_per_list())
