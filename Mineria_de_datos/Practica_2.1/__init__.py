c1c6aa566c25610572bc3faec2b4b47a3c0ec361
Gabriel Vázquez <gabvaztor@gmail.com>
2017-11-23 00:10:29 +0100

    Calculating relevants 6

--- Mineria_de_datos/Practica_2.1/__init__.py
+++ Mineria_de_datos/Practica_2.1/__init__.py
@@ -75,6 +75,7 @@ data = create_columns_per_list()
 
 def calculate_subsets_sx(array, possibilities, Y):
     s1, s2, s3, s4, s5 = [], [], [], [], []
+    sy1, sy2, sy3, sy4, sy5 = [], [], [], [], []
     all_rows = []
     p1_X1, p1_X2, p1_X3, p1_X4, p1_X5 = False, False , False, False, False
     add_to_all_rows = True
@@ -85,36 +86,40 @@ def calculate_subsets_sx(array, possibilities, Y):
             if add_to_all_rows:
                 to_add = list(row)+[y]
                 all_rows.append(to_add)
-            if len(all_rows) == 100:
-                add_to_all_rows = False
+                if len(all_rows) == 100:
+                    add_to_all_rows = False
             if string_feature == 'x1':
                 s1.append(list(row[1:5]))
+                sy1.append(list(row[1:5])+[y])
                 if p1_X1 is False:
                     if row[0] == possibilities[0] or row[0] == possibilities[1]:
                         p1_X1 = True
             elif string_feature == 'x2':
                 s2.append(list(np.hstack([row[0], row[2:5]])))
+                sy2.append(list(np.hstack([row[0], row[2:5]]))+[y])
                 if p1_X2 is False:
                     if row[1] == possibilities[0] or row[1] == possibilities[1]:
                         p1_X2 = True
             elif string_feature == 'x3':
                 s3.append(list(np.hstack([row[0:2], row[3:5]])))
+                sy3.append(list(np.hstack([row[0:2], row[3:5]]))+[y])
                 if p1_X3 is False:
                     if row[2] == possibilities[0] or row[2] == possibilities[1]:
                         p1_X3 = True
                 # pt(str(i + 1), sx)
             elif string_feature == 'x4':
                 s4.append(list(np.hstack([row[0:3], row[4]])))
+                sy4.append(list(np.hstack([row[0:3], row[4]]))+[y])
                 if p1_X4 is False:
                     if row[3] == possibilities[0] or row[3] == possibilities[1]:
                         p1_X4 = True
             elif string_feature == 'x5':
-
                 s5.append(list(row[0:4]))
+                sy5.append(list(row[0:4])+[y])
                 if p1_X5 is False:
                     if row[4] == possibilities[0] or row[4] == possibilities[1]:
                         p1_X5 = True
-    return all_rows, s1, s2, s3, s4, s5, p1_X1, p1_X2, p1_X3, p1_X4, p1_X5
+    return all_rows, s1, s2, s3, s4, s5, sy1, sy2, sy3, sy4, sy5
 
 
 def create_dict_p2():
@@ -124,6 +129,9 @@ def create_dict_p2():
         dict_p2[x,y] = []
     #pt("dict_p2",dict_p2)
     return dict_p2
+
+
+
 
 
 def find_relevants_information(data, Y, possibilities):
@@ -141,73 +149,69 @@ def find_relevants_information(data, Y, possibilities):
     pt(len(data[0]))
     array_data = np.asarray(data)
     # All subsets sx
-    all_rows, s1, s2, s3, s4, s5, p1_X1, p1_X2, p1_X3, p1_X4, p1_X5 = calculate_subsets_sx(array_data, possibilities,Y)
+    all_rows, s1, s2, s3, s4, s5, sy1, sy2, sy3, sy4, sy5 = calculate_subsets_sx(array_data, possibilities,Y)
     #calculate_strong_relevants(all_rows,y,possibilities)
-
     # Todo si para débil debe coger subconjuntos
     # Count numbers of times apper
-    x1_ = True
-    x2_ = True
-    x3_ = True
-    x4_ = True
-    x5_ = True
     dict_p2_x1 = create_dict_p2() # {[x,y]:[[0000][0001]...]}
     dict_p2_x2 = create_dict_p2() # {[x,y]:[[0000][0001]...]}
     dict_p2_x3 = create_dict_p2() # {[x,y]:[[0000][0001]...]}
     dict_p2_x4 = create_dict_p2() # {[x,y]:[[0000][0001]...]}
     dict_p2_x5 = create_dict_p2() # {[x,y]:[[0000][0001]...]}
-    for string_feature, possibilitie in possibilities.items():
+
+    for string_feature, possibility in possibilities.items():
         row_count = 0
+        #for row_count, row in enumerate(all_rows):
         for row in all_rows:
-            y_ = row.pop()
-            if string_feature == 'x1' and p1_X1 and x1_:
-                x1 = s1[row_count][0]
+            y_ = row[-1]
+            if string_feature == 'x1':
+                x1 = row[0]
                 dict_get = dict_p2_x1.get((x1,y_))
                 if not dict_get: # First case
-                    dict_get = s1[row_count]
+                    dict_get = row[1:5]
                     dict_p2_x1[x1,y_] = [dict_get]
                 else:
-                    dict_get.append(s1[row_count])
+                    dict_get.append(row[1:5])
                     dict_p2_x1[x1, y_] = dict_get
                 row_count += 1
-            elif string_feature == 'x2' and p1_X2 and x2_:
-                x2 = s2[row_count][1]
+            elif string_feature == 'x2':
+                x2 = row[1]
                 dict_get = dict_p2_x2.get((x2,y_))
                 if not dict_get: # First case
-                    dict_get = s2[row_count]
+                    dict_get = list(np.hstack([row[0], row[2:5]]))
                     dict_p2_x2[x2,y_] = [dict_get]
                 else:
-                    dict_get.append(s2[row_count])
+                    dict_get.append(list(np.hstack([row[0], row[2:5]])))
                     dict_p2_x2[x2, y_] = dict_get
                 row_count += 1
-            elif string_feature == 'x3' and p1_X3 and x3_:
-                x3 = s3[row_count][2]
+            elif string_feature == 'x3':
+                x3 = row[2]
                 dict_get = dict_p2_x3.get((x3,y_))
                 if not dict_get: # First case
-                    dict_get = s3[row_count]
+                    dict_get = list(np.hstack([row[0:2], row[3:5]]))
                     dict_p2_x3[x3,y_] = [dict_get]
                 else:
-                    dict_get.append(s3[row_count])
+                    dict_get.append(list(np.hstack([row[0:2], row[3:5]])))
                     dict_p2_x3[x3, y_] = dict_get
                 row_count += 1
-            elif string_feature == 'x4' and p1_X4 and x4_:
-                x4 = s4[row_count][1]
+            elif string_feature == 'x4':
+                x4 = row[1]
                 dict_get = dict_p2_x4.get((x4,y_))
                 if not dict_get: # First case
-                    dict_get = s4[row_count]
+                    dict_get = list(np.hstack([row[0:2], row[3:5]]))
                     dict_p2_x4[x4,y_] = [dict_get]
                 else:
-                    dict_get.append(s4[row_count])
+                    dict_get.append(list(np.hstack([row[0:2], row[3:5]])))
                     dict_p2_x4[x4, y_] = dict_get
                 row_count += 1
-            elif string_feature == 'x5' and p1_X5 and x5_:
-                x5 = s5[row_count][1]
+            elif string_feature == 'x5':
+                x5 = row[1]
                 dict_get = dict_p2_x5.get((x5,y_))
                 if not dict_get: # First case
-                    dict_get = s5[row_count]
+                    dict_get = list(row[0:4])
                     dict_p2_x5[x5,y_] = [dict_get]
                 else:
-                    dict_get.append(s5[row_count])
+                    dict_get.append(list(row[0:4]))
                     dict_p2_x5[x5, y_] = dict_get
                 row_count += 1
     pt("dict_p2_x1", dict_p2_x1)
@@ -216,18 +220,128 @@ def find_relevants_information(data, Y, possibilities):
     pt("dict_p2_x4", dict_p2_x4)
     pt("dict_p2_x5", dict_p2_x5)
 
+    # p1 = p(Xi=xi,Si=si)>0
+    # p2 = p(Y=y | Xi=xi, Si=si)
+    p2x1 = 0.
+    p2x2 = 0.
+    p2x3 = 0.
+    p2x4 = 0.
+    p2x5 = 0.
+    p3x1 = 0.
+    p3x2 = 0.
+    p3x3 = 0.
+    p3x4 = 0.
+    p3x5 = 0.
+    for string_feature, possibility in possibilities.items():
+        y_counts = 0
+        for i, row in enumerate(all_rows):
+            y_ = row[-1]
+            # X1
+            if string_feature == 'x1':
+                si = row[1:5]
+                number_of_times = s1.count(si)
+                if number_of_times > 0:
+                    # delvolver los indices que coinciden con el count
+                    indexs = []
+                    for index, s in enumerate(sy1):
+                        if si == s[0:4]:
+                            indexs.append(index)
+                    for index_ in indexs:
+                        y_1 = sy1[index_][-1]
+                        if y_ == y_1:
+                            y_counts += 1
+                if y_counts > 0:
+                    p2x1 = number_of_times/y_counts
+            # X2
+            elif string_feature == 'x2':
+                si = list(np.hstack([row[0], row[2:5]]))
+                number_of_times = s2.count(si)
+                if number_of_times > 0:
+                    # delvolver los indices que coinciden con el count
+                    indexs = []
+                    for index, s in enumerate(sy2):
+                        if si == s[0:4]:
+                            indexs.append(index)
+                    for index_ in indexs:
+                        y_1 = sy2[index_][-1]
+                        if y_ == y_1:
+                            y_counts += 1
+                if y_counts > 0:
+                    p2x2 = number_of_times/y_counts
+            # X3
+            elif string_feature == 'x3':
+                si = list(np.hstack([row[0:2], row[3:5]]))
+                number_of_times = s3.count(si)
+                if number_of_times > 0:
+                    # delvolver los indices que coinciden con el count
+                    indexs = []
+                    for index, s in enumerate(sy3):
+                        if si == s[0:4]:
+                            indexs.append(index)
+                    for index_ in indexs:
+                        y_1 = sy3[index_][-1]
+                        if y_ == y_1:
+                            y_counts += 1
+                if y_counts > 0:
+                    p2x3 = number_of_times / y_counts
+            # X4
+            elif string_feature == 'x4':
+                si = list(np.hstack([row[0:2], row[3:5]]))
+                number_of_times = s4.count(si)
+                if number_of_times > 0:
+                    # delvolver los indices que coinciden con el count
+                    indexs = []
+                    for index, s in enumerate(sy4):
+                        if si == s[0:4]:
+                            indexs.append(index)
+                    for index_ in indexs:
+                        y_1 = sy4[index_][-1]
+                        if y_ == y_1:
+                            y_counts += 1
+                if y_counts > 0:
+                    p2x4 = number_of_times / y_counts
+            # X5
+            elif string_feature == 'x5':
+                si = list(row[0:4])
+                number_of_times = s5.count(si)
+                if number_of_times > 0:
+                    # delvolver los indices que coinciden con el count
+                    indexs = []
+                    for index, s in enumerate(sy5):
+                        if si == s[0:4]:
+                            indexs.append(index)
+                    for index_ in indexs:
+                        y_1 = sy5[index_][-1]
+                        if y_ == y_1:
+                            y_counts += 1
+                if y_counts > 0:
+                    p2x5 = number_of_times / y_counts
+    pt("p2x1",p2x1)
+    pt("p2x2",p2x2)
+    pt("p2x3",p2x3)
+    pt("p2x4",p2x4)
+    pt("p2x5",p2x5)
+    # Dataset, column, y, x
+
+    x1_strong_relevant = calculate_strong_relevant(dict_p2_x1)
+
+def calculate_strong_relevant(dictionary):
+    """
+    dict_p2_x1 = {(x1,y): [x2,x3,x4,x5]}
+    """
     # p2 = p(Y=y | Xi=xi, Si=si)
     # p3 = p(Y=y | Si=si)
-    p2_x1 = 0
-    for xy, subsets in dict_p2_x1.items(): # 4 xy
-        p2_x1 = len(dict_p2_x1.get((xy[0],xy[1])))
-        pt("p2_x1 for (" + str(xy[0]) + "," +  str(xy[1]) + ")", p2_x1)
-        for si in subsets:
-            if xy[0] == 0 and xy[1] == 0:
-                if not si:
-                    p2_x1_00 = 0
-                else:
-                    p2_x1_00 = # Todo contar el número de veces que aparece s1 en Si
+    is_strong = False
+    y_count = 0
+    si_count = 0
+    for xy, si in dictionary.items():
+        if xy[0] == 0:
+            if si:
+                y_count = len(si)
+        if xy[0] == 1:
+            if si:
+                y_count = len(si)
+
 
 
 
