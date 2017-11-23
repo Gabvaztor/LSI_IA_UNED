a438b28f514648e10bc329cbc36a8c6b78ca6ee6
Gabriel Vázquez <gabvaztor@gmail.com>
2017-11-23 23:20:39 +0100

    Calculating relevants 7

--- Mineria_de_datos/Practica_2.1/__init__.py
+++ Mineria_de_datos/Practica_2.1/__init__.py
@@ -225,14 +225,21 @@ def find_relevants_information(data, Y, possibilities):
     # ----------------------------------------------------
     # TO calculate p1 and p2 probabilities
     # p1 = p(Xi=xi,Si=si)>0
-    # p2 = p(Y=y | Xi=xi, Si=si)
+    # p3 = p(Y=y | Si=si)
     p2x1, p2x2, p2x3, p2x4, p2x5 = 0., 0., 0. ,0. ,0.
+    p3x1y0, p3x1y1, p3x2y0, p3x2y1, p3x3y0, p3x3y1, p3x4y0, p3x4y1, p3x5y0, p3x5y1 = -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+    y_x1_values = []
+    y_x2_values = []
+    y_x3_values = []
+    y_x4_values = []
+    y_x5_values = []
     for string_feature, possibility in possibilities.items():
         y_counts = 0
         for i, row in enumerate(all_rows):
             y_ = row[-1]
             # X1
-            if string_feature == 'x1':
+            if string_feature == 'x1' and y_ not in y_x1_values:
+                y_x1_values.append(y_)
                 si = row[1:5]
                 number_of_times = s1.count(si)
                 if number_of_times > 0:
@@ -246,9 +253,18 @@ def find_relevants_information(data, Y, possibilities):
                         if y_ == y_1:
                             y_counts += 1
                 if y_counts > 0:
-                    p2x1 = number_of_times/y_counts
+                    if y_ == 0:
+                        p3x1y0 = number_of_times/y_counts
+                    elif y_ == 1:
+                        p3x1y1 = number_of_times/y_counts
+                else:
+                    if y_ == 0:
+                        p3x1y0 = 0
+                    elif y_ == 1:
+                        p3x1y1 = 0
             # X2
-            elif string_feature == 'x2':
+            elif string_feature == 'x2' and y_ not in y_x2_values:
+                y_x2_values.append(y_)
                 si = list(np.hstack([row[0], row[2:5]]))
                 number_of_times = s2.count(si)
                 if number_of_times > 0:
@@ -262,9 +278,18 @@ def find_relevants_information(data, Y, possibilities):
                         if y_ == y_1:
                             y_counts += 1
                 if y_counts > 0:
-                    p2x2 = number_of_times/y_counts
+                    if y_ == 0:
+                        p3x2y0 = number_of_times/y_counts
+                    elif y_ == 1:
+                        p3x2y1 = number_of_times/y_counts
+                else:
+                    if y_ == 0:
+                        p3x2y0 = 0
+                    elif y_ == 1:
+                        p3x2y1 = 0
             # X3
-            elif string_feature == 'x3':
+            elif string_feature == 'x3' and y_ not in y_x3_values:
+                y_x3_values.append(y_)
                 si = list(np.hstack([row[0:2], row[3:5]]))
                 number_of_times = s3.count(si)
                 if number_of_times > 0:
@@ -278,9 +303,18 @@ def find_relevants_information(data, Y, possibilities):
                         if y_ == y_1:
                             y_counts += 1
                 if y_counts > 0:
-                    p2x3 = number_of_times / y_counts
+                    if y_ == 0:
+                        p3x3y0 = number_of_times/y_counts
+                    elif y_ == 1:
+                        p3x3y1 = number_of_times/y_counts
+                else:
+                    if y_ == 0:
+                        p3x3y0 = 0
+                    elif y_ == 1:
+                        p3x3y1 = 0
             # X4
-            elif string_feature == 'x4':
+            elif string_feature == 'x4' and y_ not in y_x4_values:
+                y_x4_values.append(y_)
                 si = list(np.hstack([row[0:2], row[3:5]]))
                 number_of_times = s4.count(si)
                 if number_of_times > 0:
@@ -294,9 +328,18 @@ def find_relevants_information(data, Y, possibilities):
                         if y_ == y_1:
                             y_counts += 1
                 if y_counts > 0:
-                    p2x4 = number_of_times / y_counts
+                    if y_ == 0:
+                        p3x4y0 = number_of_times/y_counts
+                    elif y_ == 1:
+                        p3x4y1 = number_of_times/y_counts
+                else:
+                    if y_ == 0:
+                        p3x4y0 = 0
+                    elif y_ == 1:
+                        p3x4y1 = 0
             # X5
-            elif string_feature == 'x5':
+            elif string_feature == 'x5' and y_ not in y_x5_values:
+                y_x5_values.append(y_)
                 si = list(row[0:4])
                 number_of_times = s5.count(si)
                 if number_of_times > 0:
@@ -310,7 +353,15 @@ def find_relevants_information(data, Y, possibilities):
                         if y_ == y_1:
                             y_counts += 1
                 if y_counts > 0:
-                    p2x5 = number_of_times / y_counts
+                    if y_ == 0:
+                        p3x5y0 = number_of_times/y_counts
+                    elif y_ == 1:
+                        p3x5y1 = number_of_times/y_counts
+                else:
+                    if y_ == 0:
+                        p3x5y0 = 0
+                    elif y_ == 1:
+                        p3x5y1 = 0
     pt("p2x1",p2x1)
     pt("p2x2",p2x2)
     pt("p2x3",p2x3)
@@ -320,34 +371,196 @@ def find_relevants_information(data, Y, possibilities):
     # ----------------------------------------------------
     # ----------------------------------------------------
 
-    # p3 = p(Y=y | Si=si)
-    p3x1, p3x2 , p3x3, p3x4, p3x5 = 0, 0, 0, 0, 0
+
+
+    # p2 = p(Y=y | Xi=xi, Si=si)
+    p2x1_0y_0, p2x1_1y_0, p2x1_0y_1, p2x1_1y_1 = 0, 0, 0 ,0
+    p2x2_0y_0, p2x2_1y_0, p2x2_0y_1, p2x2_1y_1 = 0, 0, 0 ,0
+    p2x3_0y_0, p2x3_1y_0, p2x3_0y_1, p2x3_1y_1 = 0, 0, 0 ,0
+    p2x4_0y_0, p2x4_1y_0, p2x4_0y_1, p2x4_1y_1 = 0, 0, 0 ,0
+    p2x5_0y_0, p2x5_1y_0, p2x5_0y_1, p2x5_1y_1 = 0, 0, 0 ,0
+
+    y_x1_values = []
+    y_x2_values = []
+    y_x3_values = []
+    y_x4_values = []
+    y_x5_values = []
+    x_x1_values = []
+    x_x2_values = []
+    x_x3_values = []
+    x_x4_values = []
+    x_x5_values = []
+    for string_feature, possibility in possibilities.items():
+        xy_counts = 0
+        x_si_counts = 0
+        for i, row in enumerate(all_rows):
+            y_ = row[-1]
+            # X1
+            if string_feature == 'x1' and y_ not in y_x1_values:
+                y_x1_values.append(y_)
+                x = row[0]
+                if x not in x_x1_values:
+                    x_x1_values.append(x)
+                    si = s1[i]
+                    number_of_times = s1.count(si)
+                    if number_of_times > 0:
+                        indexs_x = [] # Contiene todos los indices donde está el xi condicional
+                        for index_x, row_x in enumerate(all_rows):
+                            if row_x[0] == x:
+                                indexs_x.append(index_x)
+                        sys = []
+                        for index in indexs_x:
+                            sys.append(sy1[index])
+                        for siy in sys:
+                            y_s = siy[-1]
+                            s_i = siy[0:4]
+                            if y_s == y_ and s_i == si:
+                                xy_counts += 1
+                            if s_i == si:
+                                x_si_counts += 1
+                        if xy_counts > 0:
+                            if x == 0 and y_ == 0:
+                                p2x1_0y_0 = xy_counts/x_si_counts
+                            elif x == 0 and  y_ == 1:
+                                p2x1_0y_1 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 0 :
+                                p2x1_1y_0 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 1:
+                                p2x1_1y_1 = xy_counts / x_si_counts
+            # X2
+            elif string_feature == 'x2' and y_ not in y_x2_values:
+                y_x2_values.append(y_)
+                x = row[0]
+                if x not in x_x2_values:
+                    x_x2_values.append(x)
+                    si = s2[i]
+                    number_of_times = s2.count(si)
+                    if number_of_times > 0:
+                        indexs_x = [] # Contiene todos los indices donde está el xi condicional
+                        for index_x, row_x in enumerate(all_rows):
+                            if row_x[0] == x:
+                                indexs_x.append(index_x)
+                        sys = []
+                        for index in indexs_x:
+                            sys.append(sy2[index])
+                        for siy in sys:
+                            y_s = siy[-1]
+                            s_i = siy[0:4]
+                            if y_s == y_ and s_i == si:
+                                xy_counts += 1
+                            if s_i == si:
+                                x_si_counts += 1
+                        if xy_counts > 0:
+                            if x == 0 and y_ == 0:
+                                p2x2_0y_0 = xy_counts/x_si_counts
+                            elif x == 0 and  y_ == 1:
+                                p2x2_0y_1 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 0 :
+                                p2x2_1y_0 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 1:
+                                p2x2_1y_1 = xy_counts / x_si_counts
+            # X3
+            elif string_feature == 'x3' and y_ not in y_x3_values:
+                y_x3_values.append(y_)
+                x = row[0]
+                if x not in x_x3_values:
+                    x_x3_values.append(x)
+                    si = s3[i]
+                    number_of_times = s3.count(si)
+                    if number_of_times > 0:
+                        indexs_x = [] # Contiene todos los indices donde está el xi condicional
+                        for index_x, row_x in enumerate(all_rows):
+                            if row_x[0] == x:
+                                indexs_x.append(index_x)
+                        sys = []
+                        for index in indexs_x:
+                            sys.append(sy3[index])
+                        for siy in sys:
+                            y_s = siy[-1]
+                            s_i = siy[0:4]
+                            if y_s == y_ and s_i == si:
+                                xy_counts += 1
+                            if s_i == si:
+                                x_si_counts += 1
+                        if xy_counts > 0:
+                            if x == 0 and y_ == 0:
+                                p2x3_0y_0 = xy_counts/x_si_counts
+                            elif x == 0 and  y_ == 1:
+                                p2x3_0y_1 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 0 :
+                                p2x3_1y_0 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 1:
+                                p2x3_1y_1 = xy_counts / x_si_counts
+            # X4
+            elif string_feature == 'x4' and y_ not in y_x4_values:
+                y_x4_values.append(y_)
+                x = row[0]
+                if x not in x_x4_values:
+                    x_x4_values.append(x)
+                    si = s4[i]
+                    number_of_times = s4.count(si)
+                    if number_of_times > 0:
+                        indexs_x = [] # Contiene todos los indices donde está el xi condicional
+                        for index_x, row_x in enumerate(all_rows):
+                            if row_x[0] == x:
+                                indexs_x.append(index_x)
+                        sys = []
+                        for index in indexs_x:
+                            sys.append(sy4[index])
+                        for siy in sys:
+                            y_s = siy[-1]
+                            s_i = siy[0:4]
+                            if y_s == y_ and s_i == si:
+                                xy_counts += 1
+                            if s_i == si:
+                                x_si_counts += 1
+                        if xy_counts > 0:
+                            if x == 0 and y_ == 0:
+                                p2x4_0y_0 = xy_counts/x_si_counts
+                            elif x == 0 and  y_ == 1:
+                                p2x4_0y_1 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 0 :
+                                p2x4_1y_0 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 1:
+                                p2x4_1y_1 = xy_counts / x_si_counts
+            # X5
+            elif string_feature == 'x5' and y_ not in y_x5_values:
+                y_x5_values.append(y_)
+                x = row[0]
+                if x not in x_x5_values:
+                    x_x5_values.append(x)
+                    si = s5[i]
+                    number_of_times = s5.count(si)
+                    if number_of_times > 0:
+                        indexs_x = [] # Contiene todos los indices donde está el xi condicional
+                        for index_x, row_x in enumerate(all_rows):
+                            if row_x[0] == x:
+                                indexs_x.append(index_x)
+                        sys = []
+                        for index in indexs_x:
+                            sys.append(sy5[index])
+                        for siy in sys:
+                            y_s = siy[-1]
+                            s_i = siy[0:4]
+                            if y_s == y_ and s_i == si:
+                                xy_counts += 1
+                            if s_i == si:
+                                x_si_counts += 1
+                        if xy_counts > 0:
+                            if x == 0 and y_ == 0:
+                                p2x5_0y_0 = xy_counts/x_si_counts
+                            elif x == 0 and  y_ == 1:
+                                p2x5_0y_1 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 0 :
+                                p2x5_1y_0 = xy_counts / x_si_counts
+                            elif x == 1 and y_ == 1:
+                                p2x5_1y_1 = xy_counts / x_si_counts
     # TODO p3
 
+    pt("p2x1_0y_0, p2x1_1y_0, p2x1_0y_1, p2x1_1y_1", [p2x1_0y_0, p2x1_1y_0, p2x1_0y_1, p2x1_1y_1])
+    pt("p2x1_0y_0, p2x1_1y_0, p2x1_0y_1, p2x1_1y_1", [p2x2_0y_0, p2x2_1y_0, p2x2_0y_1, p2x2_1y_1])
 
     # Dataset, column, y, x
-
-
-def calculate_strong_relevant(dictionary):
-    """
-    dict_p2_x1 = {(x1,y): [x2,x3,x4,x5]}
-    """
-    # p2 = p(Y=y | Xi=xi, Si=si)
-    # p3 = p(Y=y | Si=si)
-    is_strong = False
-    y_count = 0
-    si_count = 0
-    for xy, si in dictionary.items():
-        if xy[0] == 0:
-            if si:
-                y_count = len(si)
-        if xy[0] == 1:
-            if si:
-                y_count = len(si)
-
-
-
-
 
 
 possibilities = {'x1':[0,1], 'x2':[0,1], 'x3':[0,1], 'x4':[0,1], 'x5':[0,1]}
